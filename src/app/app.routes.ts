227287b862abd9d346384b8f3027637184f5d62b
Youssefh71 <youssefhourri69@gmail.com>
2024-10-02 15:15:25 +0200

    finish Reactive Form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ArticlePageComponent } from './article-page/article-page.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 
 export const routes: Routes = [
     { path: '', component: HomePageComponent },
     { path: 'article/:articleId', component: ArticlePageComponent },
-    { path: '**', component: NotFoundComponent }
+    { path: '**', component: NotFoundComponent },
+    { path: 'signUp', component: SignupFormComponent },
 ];
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ArticleComponent } from "../article/article.component";
 import { ContactFormComponent } from '../contact-form/contact-form.component';
+import { SignupFormComponent } from '../signup-form/signup-form.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [ArticleComponent, ContactFormComponent],
+  imports: [ArticleComponent, ContactFormComponent, SignupFormComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
